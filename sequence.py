46403c349a97d0f835c2f4864804ce071f4d8b82
Claudio Harringer <der.claudio@gmail.com>
2010-12-05 21:01:08 +0100

    Add sequence.py dummy class

---
+++ sequence.py
@@ -0,0 +1,20 @@
+"""
+File    sequence.py
+"""
+
+import unittest
+
+class sequence:
+    """
+    Provides helper methods used by the perm class.
+    """
+    pass
+
+class Test(unittest.TestCase):
+    def test_nothing(self):
+        pass
+ 
+if __name__ == '__main__':
+    unittest.main()
+
+# vim:expandtab:softtabstop=4:shiftwidth=4
