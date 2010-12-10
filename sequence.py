1d8b502bb57d9f89612b09329f824a10c8de048d
Claudio Harringer <der.claudio@gmail.com>
2010-12-10 23:22:26 +0100

    Add sequence

--- sequence.py
+++ sequence.py
@@ -8,24 +8,55 @@ class sequence:
     """
     Provides helper methods used by the perm class.
     """
+
     def IsSequence(obj):
-        return type(obj) == tuple
+        return (type(obj) == tuple) or (type(obj) == list)
+
+    def IsAllSequences(obj):
+        for part in obj:
+            if not sequence.IsSequence(part):
+                return False
+        return True
+
+    def IsAllIntegers(obj):
+        for part in obj:
+            if not type(part) == int:
+                return False
+        return True
+
+    def MaxElt(obj):
+        curmax = obj[0]
+        for elem in obj:
+            if elem > curmax:
+                curmax = elem
+        return curmax
+
+    def MinElt(obj):
+        curmin = obj[0]
+        for elem in obj:
+            if elem < curmin:
+                curmin = elem
+        return curmin
 
 class Test(unittest.TestCase):
     def test_MinElt(self):
-        pass
+        self.assertEqual(1, sequence.MinElt([1,3,4,2]))
+        self.assertEqual(1, sequence.MinElt((9,1,2,5)))
 
     def test_MaxElt(self):
-        pass
+        self.assertEqual(4, sequence.MaxElt([1,3,4,2]))
+        self.assertEqual(9, sequence.MaxElt((9,1,2,5)))
 
     def test_IsAllIntegers(self):
-        pass
+        self.assertTrue(sequence.IsAllIntegers((1,2,3)))
 
     def test_IsAllSequences(self):
-        pass
+        self.assertTrue(sequence.IsAllSequences(((1,2,3),(4,5))))
+        self.assertTrue(sequence.IsAllSequences([[1,4],[2,5,3]]))
 
     def test_IsSequence(self):
         self.assertTrue(sequence.IsSequence((1,2,3)))
+        self.assertTrue(sequence.IsSequence([4,5]))
 
 
 if __name__ == '__main__':
